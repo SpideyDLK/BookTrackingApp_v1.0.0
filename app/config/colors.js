a58b11a5e8c5adc8ef5658be2cb47f8870ebca6a
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2023-05-20 11:17:22 +0530

    colors defined

---
+++ app/config/colors.js
@@ -0,0 +1,13 @@
+export default {
+  buttonColor: "#4c8f4a",
+  backgroundColor: "#a2d9a2",
+  backgroundColorLight: "#e6f4ea",
+  textColor: "#203f1b",
+  primary: "#3b6d3f",
+  secondary: "#ffc857",
+  white: "#fff",
+  linkColor: "#007AFF",
+  errorBack: "#160b0b",
+  errorFont: "#f4c7c7",
+  errorIcon: "#f44336",
+};
